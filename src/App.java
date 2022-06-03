f0c96978638a503102aab3c99f8fcdf689f18404
Emerson Carvalho <8569519+brecarv@users.noreply.github.com>
2022-06-03 13:55:17 -0300

    observer pattern working well

--- src/App.java
+++ src/App.java
@@ -1,5 +1,13 @@
 public class App {
     public static void main(String[] args) {
+        var twitter = new Twitter();
+        twitter.addFollower(new UserBrazil());
+        twitter.addFollower(new UserUSA());
 
+        var tweet = new Tweet("Elon Musk", "Hello World!");
+        var tweetA = new Tweet("Elon Musk", "Well...");
+
+        twitter.tweetPosted(tweet);
+        twitter.tweetPosted(tweetA);
     }
 }
